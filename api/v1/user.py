5c81bcc870e89e55dc172346bbebae5565872606
victorjambo <victorjambo@live.com>
2018-03-05 22:43:36 +0300

    [Feature #155544855] User can perform business functions Read all users in db Read user info provided user_id Read all businesses in owned by user

--- api/v1/user.py
+++ api/v1/user.py
@@ -92,3 +92,28 @@ def logout():
     return jsonify({'msg': 'logged out'}), 200
 
 
+@app.route('/api/users', methods=['GET'])
+def read_all_users():
+    """Reads all users
+    """
+    return jsonify(users), 200
+
+
+@app.route('/api/user/<user_id>', methods=['GET'])
+def read_user(user_id):
+    """Reads user given an ID
+    if user is not provided then user current user ID
+    """
+    response = find_user_by_id(user_id)
+    if response:
+        return jsonify(response), 200
+    return jsonify({'warning': 'user does not exist'}), 404
+
+
+@app.route('/api/user/<user_id>/businesses', methods=['GET'])
+def read_user_businesses(user_id):
+    """Read all businesses owned by this user"""
+    response = find_business_by_user(user_id)
+    if response:
+        return jsonify(response), 200
+    return jsonify({'msg': 'user does not own a business'}), 204
