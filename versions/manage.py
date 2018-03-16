d118ca0c1338cec6eca5c75ea67f14661794b769
victorjambo <victorjambo@live.com>
2018-03-16 19:09:36 +0300

    [Fixes] docstring

--- versions/manage.py
+++ versions/manage.py
@@ -2,8 +2,13 @@ from versions import db
 
 
 class User(db.Model):
+    """Create table users
+    One-to-Many relationship with review and business
+    User has many businessess
+    User has many reviews
+    """
     __tablename__ = 'users'
-    
+
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(), unique=True, nullable=False)
     email = db.Column(db.String(), unique=True, nullable=False)
@@ -13,8 +18,13 @@ class User(db.Model):
 
 
 class Business(db.Model):
+    """Create table businesses
+    One-to-Many relationship with review and user
+    business belongs to user
+    business has many reviews
+    """
     __tablename__ = 'businesses'
-    
+
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(), index=True)
     location = db.Column(db.String(), index=True)
@@ -25,10 +35,19 @@ class Business(db.Model):
 
 
 class Review(db.Model):
+    """Create table reviews
+    One-to-Many relationship with user and business
+    review belongs to business
+    review belongs to user
+    """
     __tablename__ = 'reviews'
-    
+
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String())
     desc = db.Column(db.String())
-    business_id = db.Column(db.Integer, db.ForeignKey('businesses.id'), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    business_id = db.Column(
+        db.Integer,
+        db.ForeignKey('businesses.id'),
+        nullable=False
+    )
