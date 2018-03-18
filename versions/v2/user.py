847973ddf6ad54b0618e5731e7201d8a952bf4f8
victorjambo <victorjambo@live.com>
2018-03-18 04:23:58 +0300

    [Feature] Read all users

--- versions/v2/user.py
+++ versions/v2/user.py
@@ -4,21 +4,24 @@ get all businesses that belongs to a user
 get all reviews that belongs to a user
 get one user information
 """
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify
 from versions.v2.models import User
 
 
 mod = Blueprint('users_v2', __name__)
 
 
-@mod.route('/users', methods=['POST'])
-def all_users():
-    """Reads all users"""
-    data = request.get_json()
-    new_user = User(
-        username=data['username'],
-        email=data['email'],
-        password=data['password']
-    )
-    new_user.save()
-    return jsonify({'success': new_user.username}), 200
+@mod.route('/', methods=['GET'])
+def get_all_users():
+    """Read all users"""
+    users = User.query.all()
+    return jsonify(
+        [
+            {
+                'id': user.id,
+                'username': user.username,
+                'email': user.email,
+                'businesses': user.businesses
+            } for user in users
+        ]
+    ), 200
