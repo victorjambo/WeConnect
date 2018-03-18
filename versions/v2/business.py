3a675aa11130b033e6e32b167160eb432b248abb
victorjambo <victorjambo@live.com>
2018-03-18 16:17:21 +0300

    [Feature] PUT: Updates single business

--- versions/v2/business.py
+++ versions/v2/business.py
@@ -12,9 +12,28 @@ from flask import Blueprint, jsonify, request
 from versions.v2.models import Business, db, User
 from versions import login_required
 from versions.utils import biz_name_regex
+from functools import wraps
 
 
 mod = Blueprint('business_v2', __name__)
+
+
+def precheck(f):
+    """Checks if businessID is available
+    Check if business belongs to current user
+    """
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        business = Business.query.get(kwargs['businessId'])
+
+        if not business:
+            return jsonify({'warning': 'Business Not Found'}), 404
+
+        if args[0] != business.owner.id:
+            return jsonify({'warning': 'Not Allowed, you are not owner'}), 401
+
+        return f(*args, **kwargs)
+    return wrap
 
 
 @mod.route('/', methods=['GET'])
@@ -107,22 +126,52 @@ def read_business(businessId):
     return jsonify({'warning': 'Business Not Found'}), 404
 
 
+@mod.route('/<businessId>', methods=['PUT'])
+@login_required
+@precheck
+def update_business(current_user, businessId):
+    """Updates a business given a business ID
+    confirms if current user is owner of business
+    """
+    data = request.get_json()
+    business = Business.query.get(businessId)
+
+    business.name = data['name']
+    business.logo = data['logo']
+    business.location = data['location']
+    business.category = data['category']
+    business.bio = data['bio']
+
+    business.save()
+
+    if business.name == data['name']:
+        return jsonify({
+            'success': 'successfully updated',
+            'business': {
+                'id': business.id,
+                'name': business.name,
+                'logo': business.logo,
+                'location': business.location,
+                'category': business.category,
+                'bio': business.bio,
+                'owner': business.owner.username,
+                'created_at': business.created_at,
+                'updated_at': business.updated_at
+            }
+        }), 201
+
+    return jsonify({'warning': 'Business Not Updated'}), 400
+
+
 @mod.route('/<businessId>', methods=['DELETE'])
 @login_required
+@precheck
 def delete_business(current_user, businessId):
     """Deletes a business
     confirms if current user is owner of business
     """
     business = Business.query.get(businessId)
-
-    if not business:
-        return jsonify({'warning': 'Business Not Found'}), 404
-
     name = business.name
-
-    if current_user != business.owner.id:
-        return jsonify({'warning': 'Not Allowed, you are not owner'}), 401
-
     business.delete()
 
     if not db.session.query(
