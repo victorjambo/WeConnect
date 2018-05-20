5e62a99cc69928b59a15a0962275e767df84e3ed
Victor Jambo <victormutaijambo@gmail.com>
2018-05-20 11:27:48 +0000

    [Fixes] 404 endpoint

--- versions/__init__.py
+++ versions/__init__.py
@@ -82,6 +82,14 @@ def version1():
 def version2():
     """route for API documentation"""
     return render_template('version2.html')
+    
+@app.errorhandler(404)
+def page_not_found(e):
+    return jsonify({'warning': '404, Endpoint not found'}), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return jsonify({'warning': '500, Internal Server Error'}), 500
 
 
 import versions.v1.user
